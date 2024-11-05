73ae84cac60c9c2da47c40224548ce01941fb335
mungwanwoo <ansrhksdn999@gmail.com>
2024-11-05 16:26:42 +0900

    Update display.py

--- pages/display.py
+++ pages/display.py
@@ -1,10 +1,14 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+import json
 
 class Display:
     def __init__(self):
-        self.columns = ['id', 'password', 'age', 'gender', 'region', 'city', 'draw_count', 'last_login_at', 'created_at']
+        self.columns = ['id', 'password', 'age', 'gender', 'region', 'city', 'draw_count', 'last_login_date', 'created_at', 'login_at']
         
         
         if 'users' not in st.session_state:
@@ -36,15 +40,22 @@ class Display:
             "충청북도": ["괴산군", "단양군", "보은군", "옥천군", "영동군", "증평군", "진천군", "청주시", "충주시", "제천시"],
             "제주특별자치도": ["제주시", "서귀포시"]
         }
-
-    def load_data(self):
+    
+    @st.cache_data
+    def load_data(_self):
         try:
-            st.session_state.users = pd.read_csv('user_data.csv')
+            # JSON 파일을 딕셔너리 형태로 읽어오기
+            with open('data/user_data.json', 'r', encoding='utf-8') as f:
+                st.session_state.users = json.load(f)
         except FileNotFoundError:
-            st.session_state.users = pd.DataFrame(columns=self.columns)
-
-    def save_to_csv(self):
-        st.session_state.users.to_csv('user_data.csv', index=False)
+            st.session_state.users = {}  # 파일이 없다면 빈 딕셔너리로 초기화
+  
+    @st.cache_data
+    def save_to_json(_self):
+        
+        data = st.session_state.users
+        with open('data/user_data.json', 'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=2)
     
     def navigate_to(self,current_page):
         st.session_state.page = current_page
@@ -52,6 +63,7 @@ class Display:
 
     def display_signup(self):
         st.title("회원가입")
+
 
         id = st.text_input("아이디를 입력하세요:")
         password = st.text_input("비밀번호를 입력하세요:")
@@ -65,15 +77,32 @@ class Display:
         with col2:
             city = st.selectbox("거주 도시를 선택하세요:", self.regions[region])
 
-        draw_count = st.number_input("추첨 횟수를 입력하세요:", min_value=0, value=0)
-        last_login_at =datetime.now().date()
-        created_at = datetime.now()
+        # last_login_date =datetime.now().date()
+        
+        # login_at=[]
+        # current_datetime = datetime.now()
+
+        # created_at = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+        # login_at.append(current_datetime.strftime("%Y-%m-%d %H:%M:%S"))
         
 
         if st.button("회원가입"):
-            if id in st.session_state.users['id'].values:
+            if id in st.session_state.users:
                 st.error("이미 사용 중인 아이디입니다. 다른 아이디를 선택해주세요.")
+            elif len(password)<7 or len(password)>15:
+                st.error("비밀번호의 길이는 7이상 15이하 입니다")
+            elif age==None:
+                st.error("나이를 입력하세요")
+            
+
             else:
+                
+                login_at=[]
+                current_datetime = datetime.now()
+                last_login_date=current_datetime.strftime("%Y-%m-%d")
+
+                created_at = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+                
                 new_user = {
                     'id': id,
                     'password': password,
@@ -81,13 +110,14 @@ class Display:
                     'gender': gender,
                     'region': region,
                     'city' : city,
-                    'draw_count': draw_count,
-                    'last_login_at': last_login_at,
+                    'draw_count': 0,
+                    'last_login_date': last_login_date,
                     'created_at': created_at,
+                    'login_at' : login_at
                 }
-                new_index = len(st.session_state.users)
-                st.session_state.users.loc[new_index] = new_user
-                self.save_to_csv()
+                
+                st.session_state.users[id] = new_user
+                self.save_to_json()
                 
                 st.success("회원가입이 완료되었습니다!")
                 self.navigate_to('login')
@@ -99,20 +129,93 @@ class Display:
         login_password = st.text_input("비밀번호를 입력하세요:", type='password')
 
         if st.button("로그인"):
-            #로그인 정보에 입력 된 아이디가 세션스테이트 유저스에 id랑 같고 세션스테이트 유저스의 로그인정보에 해당하는 비밀번호와 입력 된 비밀번호만 일치 두조건만족하면 로그인성공
-            if login_id in st.session_state.users['id'].values and login_password == st.session_state.users.loc[st.session_state.users['id'] == login_id, 'password'].values[0]:
-                st.session_state.login_user = login_id  # 로그인한 사용자 정보 저장
-                self.navigate_to("home")  # 홈 페이지로 이동
-                st.success("로그인 성공!")
+            # 입력된 아이디가 users 딕셔너리에 있는지 확인
+            if login_id in st.session_state.users:
+                # 비밀번호 일치 여부 확인
                 
+                if login_password == st.session_state.users[login_id]['password']:
+                    # 로그인 성공
+                    st.session_state.login_user = login_id  # 로그인한 사용자 정보 저장
+                    current_datetime = datetime.now()
+                    login_at = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
 
+                    st.session_state.users[login_id]['login_at'].append(login_at)
+                    self.save_to_json()
+                    self.navigate_to("home")  # 홈 페이지로 이동
+                    st.success("로그인 성공!")
+                else:
+                    st.error("로그인 실패: 비밀번호가 잘못되었습니다.")
             else:
-                st.error("로그인 실패: 사용자 이름이나 비밀번호가 잘못되었습니다.")
-
+                st.error("로그인 실패: 사용자 이름이 존재하지 않습니다.")
         # 회원가입 페이지로 이동하는 버튼 추가
         if st.button("회원가입 페이지로 이동"):
             self.navigate_to('signup')
+    
+
+    st.markdown("""
+        <style>
+            .lotto-ball {
+                display: inline-block;
+                width: 50px;
+                height: 50px;
+                border-radius: 50%;
+                color: white;
+                text-align: center;
+                line-height: 50px;  /* 수직 정렬 */
+                font-size: 20px;
+                margin: 10px;
+                box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+            }
+            .lotto-container {
+                display: flex;
+                justify-content: center; /* 가운데 정렬 */
+                margin-bottom: 20px;
+            }
+        </style>
+        """, unsafe_allow_html=True)
+
+    # 색상 지정 함수
+    def get_color(self,number):
+        
+        if 1 <= number <= 10:
+            return "#f9c74f"  # 노란색
+        elif 11 <= number <= 20:
+            return "#007bff"  # 파란색
+        elif 21 <= number <= 30:
+            return "#dc3545"  # 빨간색
+        elif 31 <= number <= 40:
+            return "#6c757d"  # 회색
+        elif 41 <= number <= 45:
+            return "#28a745"  # 녹색
+        else:
+            return "#000000"  # 기본 색상
+
+    def display_lotto_numbers(self,numbers):
+        lotto_balls_html = '<div class="lotto-container">'
+        for value in numbers:
+            color = self.get_color(value)
+            lotto_balls_html += f'<div class="lotto-ball" style="background-color: {color};">{value}</div>'
+        lotto_balls_html += '</div>'
+        st.markdown(lotto_balls_html, unsafe_allow_html=True)
+    
+
+
+
     def display_home(self):
+        lotto_data = pd.read_csv('lotto_dict.csv')
+
+        # 딕셔너리로 변환
+        lotto_dict = {}
+        for _, row in lotto_data.iterrows():
+            round_name = row['round']
+            numbers = row[['num1', 'num2', 'num3', 'num4', 'num5', 'num6']].tolist()
+            if round_name not in lotto_dict:
+                lotto_dict[round_name] = []
+            lotto_dict[round_name].append(numbers)
+
+        # 딕셔너리 출력
+        
+
         
         # '''
         # 메인화면 우측 맨위 로그인버튼과 회원가입버튼
@@ -132,7 +235,7 @@ class Display:
         #    고객의 추첨횟수는 기본값 0으로설정
         #    나이는 최솟값과 최댓값 설정 1~100
         #    로그인기록은 데이트타임으로 받기
-        #                     streamlit run pages\sign_up.py
+        #                     streamlit run pages\display.py
         
 
         # 로그인이 되어 있다면 매 회차 10회 추첨 결과를 화면에출력
@@ -150,6 +253,7 @@ class Display:
         with col2:
             if 'login_user' in st.session_state:
                 st.write(f"id: {st.session_state.login_user}")
+                
             # 내일 로그인중이면 디스에이블하기위한것
             #누르면 로그인페이지로 이동
             elif st.button("로그인"):
@@ -168,10 +272,16 @@ class Display:
             
                 
         st.title("로또 번호 추첨 페이지")
+        
+        for round,nums in lotto_dict.items():
+            st.write(f"{round}회차 당첨번호")
+            for num in nums:
+                self.display_lotto_numbers(num)
+               
+               
+            
 
-
-
-                
+            
 
     
 if __name__ == "__main__":
