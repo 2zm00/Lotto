f0c093e6201edfc9ccde2d8f00db36e842cf444d
2zm00 <ljm010301@naver.com>
2024-11-06 17:27:37 +0900

    프로토컬 수정

--- pages/map.py
+++ pages/map.py
@@ -40,7 +40,7 @@ def show_map():
         # 지도를 표시할 HTML 템플릿
         map_html = f"""
         <div id="map" style="width:100%;height:600px;"></div>
-        <script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey={kakao_key}"></script>
+        <script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey={kakao_key}"></script>
         <script>
             window.onload = function() {{
                 var container = document.getElementById('map');
